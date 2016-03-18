bd83a45c247b45c1c9c18a0952cd3f84a52f8841
kotAndEgor <kit4curs@gmail.com>
2016-03-18 21:13:38 +0500

    dialog

--- app/src/main/java/com/home/installationofdoors/ShowDBActivity.java
+++ app/src/main/java/com/home/installationofdoors/ShowDBActivity.java
@@ -1,15 +1,21 @@
 package com.home.installationofdoors;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -55,9 +61,36 @@ public class ShowDBActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+
             case R.id.buttonAddProfile:
                 Log.d("myLog", "Добавление нового профиля");
+                View view = LayoutInflater.from(ShowDBActivity.this).inflate(R.layout.user_input, null);
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(ShowDBActivity.this);
+                alertBuilder.setView(view);
+
+                /*определение всех компонентов управления в диалоге*/
+                final EditText nameProfile = (EditText)findViewById(R.id.nameProfile);
+                final EditText widthProfile = (EditText)findViewById(R.id.widthProfile);
+                final EditText xvaluex = (EditText)findViewById(R.id.xvaluex);
+                final EditText valueRollers = (EditText)findViewById(R.id.valueRollers);
+                final EditText valueTolerance = (EditText)findViewById(R.id.valueTolerance);
+                final EditText jumperMagnitude = (EditText)findViewById(R.id.jumperMagnitude);
+                alertBuilder.setCancelable(true).setPositiveButton("Ок", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(nameProfile.getText().toString().equals("")||widthProfile.getText().toString().equals("")||
+                                xvaluex.getText().toString().equals("")||valueRollers.getText().toString().equals("")||
+                                valueTolerance.getText().toString().equals("")||jumperMagnitude.getText().toString().equals("")){
+                            Toast.makeText(ShowDBActivity.this, "Заполните все поля!", Toast.LENGTH_SHORT);
+                        }else{
+
+                        }
+                    }
+                });
+                Dialog dialog = alertBuilder.create();
+                dialog.show();
                 break;
+
             case R.id.buttonDelProfile:
                 Log.d("myLog", "Удаление нового профиля");
                 break;
