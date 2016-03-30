8d331868ac5838b3e4ea69832ab1feabdc87b36a
kotAndEgor <kit4curs@gmail.com>
2016-03-30 21:57:47 +0500

    name radio button exist

--- app/src/main/java/com/home/installationofdoors/ResultActivity.java
+++ app/src/main/java/com/home/installationofdoors/ResultActivity.java
@@ -3,6 +3,7 @@ package com.home.installationofdoors;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 
@@ -13,6 +14,10 @@ public class ResultActivity extends AppCompatActivity {
 
     private TextView textViewHeightDoor, textViewWidthDoor,
                     textViewInstHeight, textViewInstWidth;
+
+    private RadioButton rbWithoutGlass, rbFree, rbHalf,
+                        rbOneThird, rbOneQuarter, rbOneQuarter2,
+                        rbOneFifth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +31,20 @@ public class ResultActivity extends AppCompatActivity {
         textViewInstHeight = (TextView)findViewById(R.id.textViewInstHeight);
         textViewInstWidth = (TextView)findViewById(R.id.textViewInstWidth);
 
-
         /*занесение результатов в лэйблы*/
         textViewHeightDoor.setText("Высота двери: " + MainActivity.calc.getHeight());
         textViewWidthDoor.setText("Ширина двери: " + MainActivity.calc.getWidth());
         textViewInstHeight.setText("Высота вставки: " + MainActivity.calc.getInsertHeight());
         textViewInstWidth.setText("Ширина вставки: " + MainActivity.calc.getInsertWidth());
+
+        /*определение radio button*/
+        rbWithoutGlass = (RadioButton)findViewById(R.id.radioButtonWithoutGlass);
+        rbFree = (RadioButton)findViewById(R.id.radioButtonFree);
+        rbHalf = (RadioButton)findViewById(R.id.radioButtonHalf);
+        rbOneThird = (RadioButton)findViewById(R.id.radioButtonOneThird);
+        rbOneQuarter = (RadioButton)findViewById(R.id.radioButtonOneQuarter);
+        rbOneQuarter2 = (RadioButton)findViewById(R.id.radioButtonOneQuarter2);
+        rbOneFifth = (RadioButton)findViewById(R.id.radioButtonOneFifth);
 
 
     }
